b1fc8c3e088d5a088fc25ae13d84e5eba14d05fe
Carlos Alberto da Costa Pinto Neto <caneto@gmail.com>
2022-10-23 12:02:58 -0300

    Atualização

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,5 +1,10 @@
 import 'dart:ui';
 
 const green1 = Color(0xff53B175);
+const blacktext = Color(0xFF181725);
 const bgColor = Color(0xffe4ecfa);
-const lightgrey = Color(0xffb8c6e6);
+const whiteIcon = Color(0xffFFF9FF);
+const lightgrey = Color(0xffB1B1B1);
+const textgrey = Color(0xff7C7C7C);
+const blueGoogle = Color(0xff5383EC);
+const blueFacebook = Color(0xff4A66AC);
